8f5f9c9053238aa4f3a8ea84a7075ef92e767759
ChristianGiuseppe <nocerino.christian.cn@gmail.com>
2022-10-29 00:50:43 +0200

    init project devnote

---
+++ Devnote/Devnote/DevnoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  DevnoteApp.swift
+//  Devnote
+//
+//  Created by Christian Nocerino on 29/10/22.
+//
+
+import SwiftUI
+
+@main
+struct DevnoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
