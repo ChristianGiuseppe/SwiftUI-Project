150af389b55248f74068de6cd09b85c2b2d21b0d
ChristianGiuseppe <nocerino.christian.cn@gmail.com>
2022-11-09 11:43:55 +0100

    init project todo app

---
+++ Todo App/Todo App/Todo_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Todo_AppApp.swift
+//  Todo App
+//
+//  Created by Christian Nocerino on 09/11/22.
+//
+
+import SwiftUI
+
+@main
+struct Todo_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
